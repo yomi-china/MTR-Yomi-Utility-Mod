f5d9c35af4e2f74d71b2aaa8de80c8b7b26fe7e0
yomi_307 <yomi2023@qq.com>
2026-06-09 20:43:27 +0800

    1.4.0正式版！

--- src/main/java/com/yomi/mtryum/render/RendererLiftArrivalLight.java
+++ src/main/java/com/yomi/mtryum/render/RendererLiftArrivalLight.java
@@ -2,10 +2,6 @@ package com.yomi.mtryum.render;
 
 import com.yomi.mtryum.block.LiftArrivalLightBlock;
 
-/**
- * 电梯到站灯具体渲染器。
- * 继承 AbstractRendererLiftArrivalLight，绑定具体的 Entity 类型。
- */
 public class RendererLiftArrivalLight extends RendererAbstractLiftArrivalLight<LiftArrivalLightBlock.Entity> {
 
     public RendererLiftArrivalLight() {
